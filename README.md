7e3698f09c5f57f97d50e7ae3fbcaf02a8ecddde
Kenzy Louis <klouis@cognitivescale.com>
2019-09-29 00:17:03 -0500

    00 - Intial commit - starting point

---
+++ README.md
@@ -0,0 +1,10 @@
+create folder
+create venv
+git init
+pip install requirements
+create .gitignore
+create .flaskenv
+create settings.py
+create application.py
+create manage.py
+blueprints
---
+++ application.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import flask_SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_pyfile('settings.py')
+
+    db.init_app()
+    migrate = Migrate(app, db)
+
+    return app
